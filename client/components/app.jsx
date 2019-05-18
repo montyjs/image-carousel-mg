a247b294f6f44017a7d752870c9454cb205d9a2a
Morgan Galvin <morgan@ludwigthepig.com>
2019-05-17 18:29:13 -0600

    added circleci file

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -1,29 +1,22 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 
-function App() {
+class App extends React.component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0
+    }
+    this.fetchProducts = this.fetchProducts.bind(this);
+    this.fetchImages = this.fetchImages.bind(this);
+  }
   // Declare a new state variable, which we'll call "count"
-  const [count, setCount] = useState(0);
-  /**
-   * ++++++++++++++++++++++++++++++++++++++++++++++++++++++
-   * ++++ https://reactjs.org/docs/hooks-overview.html ++++
-   * ++++++++++++++++++++++++++++++++++++++++++++++++++++++
-   * When you call useEffect, you’re telling React to run your
-   * “effect” function after flushing changes to the DOM. Effects
-   * are declared inside the component so they have access to its
-   * props and state. By default, React runs the effects after
-   * every render — including the first render.
-   */
-  useEffect(() => {
-    document.title = `${count} click`;
-  });
-
-  function fetchImages() {
+  fetchImages() {
     return fetch('/images')
       .then(res => res.json())
       .then(data => console.log(data))
       .catch(err => console.log(err));
   }
-  function fetchProducts() {
+  fetchProducts() {
     return fetch('/products')
       .then(res => res.json())
       .then(data => console.log(data.rows))
@@ -36,17 +29,11 @@ function App() {
         <img src="https://s3-us-west-2.amazonaws.com/fec-rei/kiwi/back_kiwi_tarantulace_kiwigrey_5_8.jpg" alt="tarantulace" />
       </div>
       <div className="buy-wrapper">
-        <p>
-          {`You clicked ${count} times`}
-        </p>
         <button type="button" onClick={() => fetchProducts()}>
           Console Log Products
         </button>
         <button type="button" onClick={() => fetchImages()}>
           Console Log Images
-        </button>
-        <button type="button" onClick={() => setCount(count + 1)}>
-          Click me
         </button>
       </div>
     </div>
