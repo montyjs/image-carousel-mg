ed1f7d72cdb96e846fe6bb17ca803f742e402589
Morgan Galvin <morgan@ludwigthepig.com>
2019-05-20 16:25:07 -0600

    first pass on checkout css

--- client/components/checkout.jsx
+++ client/components/checkout.jsx
@@ -15,23 +15,46 @@ const Checkout = ({
         { productName }
       </h1>
     </div>
-    <div>
-      { itemNumber }
+    <div className="product-info-row">
+      <div className="stars">
+        add some starz
+      </div>
+      <div className="no-ratings">
+        { `${rating}(${noRatings})` }
+      </div>
+      <div className="item-number">
+        { `item #${itemNumber}` }
+      </div>
     </div>
-    <div>
-      { color }
+    <div className="product-current-price">
+      { `$${price}` }
+    </div>
+    <div className="message-wrapper">
+      <button type="button" className="product-shipping-button">
+        <span>
+          {/* The non-solid truck is not included in the free version of font-awesome */}
+          <i className="fas fa-truck" />
+        </span>
+        This item ships for FREE!
+      </button>
+      <p className="price-messaging">
+        <b>Members save 20%</b>
+        {' '}
+on one eligible full-price item with code
+        {' '}
+        <b>ANNV19</b>
+        {' '}
+thru May 27.
+        {' '}
+        <span className="details-link">Details</span>
+      </p>
+    </div>
+    <div className="color-wrapper">
+      { `Color: ${color.slice(0, 1).toUpperCase() + color.slice(1)}` }
+      <img src="https://rei.com/media/4f72b233-00f4-449b-b3c8-c338b526cb05?size=60" className="shoe-thumbnail" alt="shoe-thumbnail" />
     </div>
     <div>
       { thumbnailUrl }
-    </div>
-    <div>
-      { price }
-    </div>
-    <div>
-      { rating }
-    </div>
-    <div>
-      { noRatings }
     </div>
   </div>
 );
@@ -41,7 +64,7 @@ Checkout.propTypes = {
   companyName: PropTypes.string,
   itemNumber: PropTypes.number,
   color: PropTypes.string,
-  thumbnailUrl: PropTypes.number,
+  thumbnailUrl: PropTypes.string,
   price: PropTypes.number,
   rating: PropTypes.number,
   noRatings: PropTypes.number,
