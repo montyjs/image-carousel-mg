8bac621209f0b49e817e77fc8928b40f2b24f142
Morgan Galvin <morgan@ludwigthepig.com>
2019-05-24 11:15:43 -0600

    added tests for productInfo component

---
+++ tests/mediaWrapper/imageInfo.test.jsx
@@ -0,0 +1,37 @@
+/* eslint-disable no-undef */
+import React from 'react';
+import { shallow } from 'enzyme';
+import ImageInfo from '../../client/components/imageSubComponents/imageInfo';
+
+const mockImage = {
+  color: 'flame',
+  index: 0,
+  orientation: 'inuse_3',
+  count: 6,
+};
+// color, index, orientation, count,
+
+describe('Rendering', () => {
+  it('renders without crashing', () => {
+    const wrap = shallow(<ImageInfo {...mockImage} />);
+    expect(wrap.exists()).toBe(true);
+  });
+  it('should have a span element', () => {
+    const wrap = shallow(<ImageInfo {...mockImage} />);
+    expect(wrap.find('span')).toHaveLength(1);
+  });
+});
+
+describe('Display Props', () => {
+  const wrap = shallow(<ImageInfo {...mockImage} />);
+  it('should not render the index + 1 (no zero index)', () => {
+    expect(wrap.contains(<p>
+Image 1 of 6
+{' '}
+<span>Inuse view (flame)</span>
+</p>)).toBe(true);
+  });
+  it('does not display underscores or numbers and capitalize the first letter of the orientation', () => {
+    expect(wrap.contains(<span>Inuse view (flame)</span>)).toBe(true);
+  });
+});
--- tests/mediaWrapper/mediaWrapper.test.jsx
+++ tests/mediaWrapper/mediaWrapper.test.jsx
@@ -23,3 +23,25 @@ describe('Rendering', () => {
     expect(wrap.contains('<img alt="flame from the inuse_3" src="https://s3-us-west-2.amazonaws.com/fec-rei/flame/inuse_3_full.jpg"></img>'));
   });
 });
+
+describe('Invoking Zoom Function', () => {
+  it('should call a function when mouseenters activeImage', () => {
+    const mockFn = jest.fn();
+    const wrap = shallow(<MediaWrapper
+      activeImage={mockImage}
+      handlers={{ handleZoom: mockFn }}
+    />);
+    wrap.find('img').simulate('mouseenter');
+    expect(mockFn).toHaveBeenCalled();
+  });
+
+  it('should call a function when activeImage is focused', () => {
+    const mockFn = jest.fn();
+    const wrap = shallow(<MediaWrapper
+      activeImage={mockImage}
+      handlers={{ handleZoom: mockFn }}
+    />);
+    wrap.find('img').simulate('focus');
+    expect(mockFn).toHaveBeenCalled();
+  });
+});
